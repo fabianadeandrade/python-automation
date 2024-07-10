569f74eb58acc2ab3bb022769cde0c6b53f95569
fabianadeandrade <G00425707@atu.ie>
2024-07-10 19:08:55 +0100

    creating automation robot in python

---
+++ code.py
@@ -0,0 +1,6 @@
+# step one - login at the company's login
+#  Link: https://dlp.hashtagtreinamentos.com/python/intensivao/login
+# step two: login
+# step three: import/export the database
+# step four: register product
+# step five: Repeat step four until all products are registered
