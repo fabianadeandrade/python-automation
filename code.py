d013dd3f1434f24878eb83f2a051a1e05fabc843
fabianadeandrade <G00425707@atu.ie>
2024-07-11 19:43:46 +0100

    added new file

--- code.py
+++ code.py
@@ -1,10 +1,18 @@
 import pyautogui
+import time
 
 pyautogui.PAUSE = 0.5
 
+# step one - login at the company's login
+#  Link: https://dlp.hashtagtreinamentos.com/python/intensivao/login
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
 
 pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.press("enter")
+
+time.sleep(3)
+
+# step two: login
+pyautogui.click
